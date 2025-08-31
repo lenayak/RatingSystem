c6ce051b5c21943f0f149c4b5f78f5a524ada137
lenayak <lenayak16@gmail.com>
2025-08-31 16:57:28 +0400

    in progress

--- src/main/java/com/example/demo/Services/RestaurantService.java
+++ src/main/java/com/example/demo/Services/RestaurantService.java
@@ -7,7 +7,7 @@ import com.example.demo.Repositories.RestaurantRepository;
 import org.springframework.stereotype.Service;
 
 // import java.math.BigDecimal;
-import java.util.List;
+// import java.util.List;
 
 @Service  
 public class RestaurantService {
@@ -19,26 +19,6 @@ public class RestaurantService {
 
     public Restaurant save(Restaurant restaurant) {
         return restaurantRepository.save(restaurant);
-    }
-
-    public boolean remove(Long id) {
-        return restaurantRepository.remove(id);
-    }
-
-    public List<Restaurant> findAll() {
-        return restaurantRepository.findAll();
-    }
-
-    public List<RestaurantResponseDTO> getAllRestaurants() {
-        return  restaurantRepository.findAll().stream()
-            .map(this::convertToResponse)
-            .toList();
-    }
-
-    public RestaurantResponseDTO getRestaurantById(Long id) {
-        Restaurant restaurant = restaurantRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Ресторан не найден"));
-        return convertToResponse(restaurant);
     }
 
     public RestaurantResponseDTO saveRestaurant(RestaurantRequestDTO restaurantRequestDTO) {
@@ -57,4 +37,3 @@ public class RestaurantService {
         restaurantRepository.remove(id);
     }
 }
-
